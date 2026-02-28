275dade29e50aba12c6ce682039bcca938b166cc
sharmaayush89050-beep <sharmaayush89050@gmail.com>
2026-02-28 12:30:53 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
-function testConnection(){
-    console.log("App.js Loaded Successfully");
+let cart = [];
+let total = 0;
+
+function addToCart(name, price){
+
+    cart.push({name, price});
+    total += price;
+
+    updateCart();
+
+}
+
+function updateCart(){
+
+    const cartList = document.getElementById("cart-items");
+    const totalSpan = document.getElementById("total");
+
+    cartList.innerHTML = "";
+
+    cart.forEach(item=>{
+        let li = document.createElement("li");
+        li.textContent = item.name + " - ₹" + item.price;
+        cartList.appendChild(li);
+    });
+
+    totalSpan.textContent = total;
+
 }
